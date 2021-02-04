d8ba207eeb4b031fa171272818357fe3c6459660
creativecoder <creativecoder@users.noreply.github.com>
2021-02-04 17:29:33 +0000

    Dialogue block: Add a save function for fallback content (#18598)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/537885738

--- _inc/blocks/map/view.asset.php
+++ _inc/blocks/map/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-token-list'), 'version' => '3440aa7bb6e0f4956b494643de9e1b5a');
+<?php return array('dependencies' => array('lodash', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-token-list'), 'version' => 'b14755b9598a9f142fdcd4a9ac94008c');
