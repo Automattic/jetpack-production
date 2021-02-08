37169b53afc4f1cdf5d73dbb743f57cf16351927
jeherve <jeherve@users.noreply.github.com>
2021-02-08 14:43:31 +0000

    Update dependency focus-trap to v6.3.0 (#18515)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/548275630

--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '34e6969c43fdd1b8b79538e6c3633343');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'af0fb6ff21b539dfb73a190b8485bf6e');
