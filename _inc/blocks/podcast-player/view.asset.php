683fa02247cc11145b6d3b55f7121f7cd4070869
jeherve <jeherve@users.noreply.github.com>
2021-03-04 17:20:43 +0000

    URL-encode tag markup in SVG data: URLs (#18981)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/621786656

--- _inc/blocks/podcast-player/view.asset.php
+++ _inc/blocks/podcast-player/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-a11y', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'daa78a615eaaa6b3b2ab20f9b0c3133b');
+<?php return array('dependencies' => array('lodash', 'wp-a11y', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '1f1f04181cd9289d87db4bdb070a834e');
