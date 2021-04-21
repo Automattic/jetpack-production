6be7cef8f446862491ed01db7b9c613a1e3cd739
kraftbj <kraftbj@users.noreply.github.com>
2021-04-21 17:55:25 +0000

    CLI: Add a root-level install command (#19606)
    * Add a yarn cli-install command
    
    * Update documentation
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/771544046

--- vendor/automattic/jetpack-password-checker/vendor/autoload.php
+++ vendor/automattic/jetpack-password-checker/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitaad12de230849fdd05710918abc4ccc9::getLoader();
+return ComposerAutoloaderInit6361db62f62c086fd009433a93e78cae::getLoader();
