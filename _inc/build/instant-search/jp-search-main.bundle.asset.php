db6ae236271b25586acd844965d55d661c7f077d
unDemian <unDemian@users.noreply.github.com>
2021-06-25 07:50:43 +0000

    Instant Search: support multi site search results (#19939)
    Co-authored-by: Jason Moon <4044428+jsnmoon@users.noreply.github.com>
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    Co-authored-by: Jason Moon <jason.moon@automattic.com>
    Co-authored-by: Brandon Kraft <public@brandonkraft.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/970663386

--- _inc/build/instant-search/jp-search-main.bundle.asset.php
+++ _inc/build/instant-search/jp-search-main.bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n', 'wp-polyfill'), 'version' => '9822ba018603d581bf5d2c776c0d1f2d');
+<?php return array('dependencies' => array('wp-i18n', 'wp-polyfill'), 'version' => '07469fe2d35eceab490aa8aae04165e5');
