076c18256e23b6bfff7a3ad45a5110e09b2dc923
kraftbj <kraftbj@users.noreply.github.com>
2021-09-03 20:44:33 +0000

    [not verified] RP: Use all args for the filter (#20953)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1199332236

--- vendor/composer/jetpack_autoload_classmap.php
+++ vendor/composer/jetpack_autoload_classmap.php
@@ -110,6 +110,10 @@ return array(
 		'version' => '1.4.7.0',
 		'path'    => $vendorDir . '/automattic/jetpack-config/src/class-config.php'
 	),
+	'Automattic\\Jetpack\\Abtest' => array(
+		'version' => '1.9.10.0',
+		'path'    => $vendorDir . '/automattic/jetpack-abtest/src/class-abtest.php'
+	),
 	'Jetpack_Sync_Modules' => array(
 		'version' => '1.6.5.0',
 		'path'    => $vendorDir . '/automattic/jetpack-compat/legacy/class-jetpack-sync-modules.php'
@@ -145,10 +149,6 @@ return array(
 	'Automattic\\Jetpack\\Backup\\Helper_Script_Manager' => array(
 		'version' => '1.1.3.0',
 		'path'    => $vendorDir . '/automattic/jetpack-backup/src/class-helper-script-manager.php'
-	),
-	'Automattic\\Jetpack\\Abtest' => array(
-		'version' => '1.9.10.0',
-		'path'    => $vendorDir . '/automattic/jetpack-abtest/src/class-abtest.php'
 	),
 	'Automattic\\Jetpack\\Partner' => array(
 		'version' => '1.5.2.0',
