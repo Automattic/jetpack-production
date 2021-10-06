9e799257b98b8b92fbdffe51b41cf8dee9ad6fdc
kallehauge <kallehauge@users.noreply.github.com>
2021-10-06 15:34:07 +0000

    Jetpack Plugin: Unify money-back guarantee component (#21189)
    * Money back guarantee: add solid style and different product frequency
    
    * Money back guarantee: center align frequency number
    
    * Money back guarantee: Reduce space between icon and text
    
    * Simplify component to only change looks
    
    * Correct existing implementation of component
    
    * Jetpack Plugin: Rename Moneybackguarantee yearly icon
    
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1312489036

--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '7c1fee7bd2eb4daa1251d215caa3ec17');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'd5fed9a98a2397c5181c9c8015546ff3');
