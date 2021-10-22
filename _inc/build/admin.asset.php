f0c535ebdab43155e8257e9bfefabed4f942c273
jeherve <jeherve@users.noreply.github.com>
2021-10-22 09:28:51 +0000

    Settings: Show backups-only settings card for tiered backup products (#21500)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1371548807

--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '36b680de8762463a5b6e0d0a56953d01');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '5e0613072daf665921114daaf6c17acb');
