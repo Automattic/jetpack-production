6a4630ea55d410ba71a14bff95ac44f2dd4ea595
jeherve <jeherve@users.noreply.github.com>
2021-11-25 18:50:51 +0000

    ConnectScreen: make button full width on small screens (#21683)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1504915076

--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '098ec718c58d08dd8eb9343377ca0597');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '6d637289661a2080af1e65bd25753ff2');
