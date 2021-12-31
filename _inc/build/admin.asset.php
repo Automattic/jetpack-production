e4550f830ae687f78e5e2fa284a1a14152a1d732
samiff <samiff@users.noreply.github.com>
2021-12-31 22:39:55 +0000

    Jetpack Licenses: Add links to license activation screen (#22169)
    * Add links to license activation success screen
    
    * Add changelog
    
    * Remove unused function
    
    * Prevent widow words by replacing white space with &nbsp;
    
    * Stack buttons horizontally aligned on mobile
    
    * Fix spacing
    
    Co-authored-by: Renzo Canepa <rcanepag@gmail.com>
    Co-authored-by: Samiff <samiff@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1641877581

--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '9d0d8fd0625660354219c29f218699eb');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'c644596138c3a8e2b7e2ca2a9f784976');
