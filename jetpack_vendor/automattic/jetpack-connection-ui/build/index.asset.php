6f9a0168798d90897a3fcb703cda34572dbc88cf
jwebbdev <jwebbdev@users.noreply.github.com>
2022-01-15 02:39:35 +0000

    Fixed style for button on Connection Required Plan screen (#22361)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1700424997

--- jetpack_vendor/automattic/jetpack-connection-ui/build/index.asset.php
+++ jetpack_vendor/automattic/jetpack-connection-ui/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '1f86d10a7f2605614f1bd1523f17ad1c');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '10b3a031ffc700e41f548def250aafc1');
