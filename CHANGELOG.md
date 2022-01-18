47836dcee90e17b2f9b76ccb3967134df09f122a
kraftbj <kraftbj@users.noreply.github.com>
2022-01-18 22:01:40 +0000

    Dashboard: update CTAs (wording, layout) in the AAG section. (#22340)
    * Dashboard: update CTAs (wording, layout) in the AAG section.
    
    Internal reference: p8oabR-LV-p2#comment-5865
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1714977161

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ This is an alpha version! The changes listed here are not final.
 - Jetpack Tiled Gallery Block: Port part of the block to native mobile (Gutenberg Mobile).
 
 ### Other changes <!-- Non-user-facing changes go here. This section will not be copied to readme.txt. -->
+- Dashboard: update CTAs (wording, layout) in the At A Glance section.
 - Fixed development mode prop type warning in UserLicenseActivationNotice component.
 - Fix PHPCS sniffs in root classes.
 - Updated package dependencies.
--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'cbd7a35b28814638a5c24ab6937877c6');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '4e2cbff76afa3f0daa23b0a6acd46b36');
