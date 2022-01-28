8fe1a4b728221e383b1e1f0a6f522cea8973692e
jeherve <jeherve@users.noreply.github.com>
2022-01-28 17:16:33 +0000

    Instant Search: add image alt text from API (#22295)
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1762680613

--- jetpack_vendor/automattic/jetpack-search/build/customberg/jp-search-configure.asset.php
+++ jetpack_vendor/automattic/jetpack-search/build/customberg/jp-search-configure.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url', 'wp-viewport'), 'version' => 'cb3afa8b29f81769266a1b2d239f6374');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url', 'wp-viewport'), 'version' => '82916a031c9be4d3c7cab0e7ba357cc2');
--- jetpack_vendor/automattic/jetpack-search/build/instant-search/jp-search.asset.php
+++ jetpack_vendor/automattic/jetpack-search/build/instant-search/jp-search.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n', 'wp-jp-i18n-loader', 'wp-polyfill'), 'version' => '85aa6312819011fa991e15e1b8b67873');
+<?php return array('dependencies' => array('wp-i18n', 'wp-jp-i18n-loader', 'wp-polyfill'), 'version' => '799af5da4f03394938d52f5e5dab4dce');
