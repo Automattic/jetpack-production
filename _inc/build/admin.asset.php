18224ace8f9ad9f3bcf41de37f549355758c774e
oskosk <oskosk@users.noreply.github.com>
2022-01-28 13:14:19 +0000

    Dashboard: Update Apps Card to not require utm_source prop (#22545)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1761621223

--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '81cbd33411df74bd5916fc4af764ab71');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '426cd2fa5e9103d68711a803164a6d79');
