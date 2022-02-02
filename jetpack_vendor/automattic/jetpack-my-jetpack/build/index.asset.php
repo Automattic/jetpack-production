e77a4e4fe375c320840c40110d8f4b88d772ca74
oskosk <oskosk@users.noreply.github.com>
2022-02-02 21:57:41 +0000

    My Jetpack: Fire Tracks events for click on product add link (#22629)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1786345879

--- jetpack_vendor/automattic/jetpack-my-jetpack/build/index.asset.php
+++ jetpack_vendor/automattic/jetpack-my-jetpack/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '49a6b6d1bbaa5bb1caf479f2ce8e61a0');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'b29ba2a81aa902a722e4c3a7ca3fb8bc');
