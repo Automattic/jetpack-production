075504d49aa65680866a99350d4671ff6e4aef08
samiff <samiff@users.noreply.github.com>
2022-03-08 17:59:45 +0000

    Updated package versions for automattic/jetpack
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1952977122

--- vendor/composer/jetpack_autoload_filemap.php
+++ vendor/composer/jetpack_autoload_filemap.php
@@ -7,23 +7,23 @@ $baseDir   = dirname($vendorDir);
 
 return array(
 	'3773ef3f09c37da5478d578e32b03a4b' => array(
-		'version' => '1.17.9999999.9999999-dev',
+		'version' => '1.17.6.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-assets/actions.php'
 	),
 	'd4eb94df91a729802d18373ee8cdc79f' => array(
-		'version' => '1.2.9999999.9999999-dev',
+		'version' => '1.2.5.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-backup/actions.php'
 	),
 	'009de6aaa0d497eacea41fab13fc05f1' => array(
-		'version' => '1.6.9999999.9999999-dev',
+		'version' => '1.6.8.0',
 		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-compat/functions.php'
 	),
-	'c41c8291342c1bc5fcb32cf6fda33a14' => array(
-		'version' => '0.11.9999999.9999999-dev',
-		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-search/search.php'
-	),
 	'd7b298d70ab0e8605f5d5b6edeab1bd2' => array(
-		'version' => '0.1.9999999.9999999-dev',
+		'version' => '0.1.0.0',
 		'path'    => $vendorDir . '/automattic/jetpack-waf/actions.php'
+	),
+	'c41c8291342c1bc5fcb32cf6fda33a14' => array(
+		'version' => '0.11.0.0',
+		'path'    => $baseDir . '/jetpack_vendor/automattic/jetpack-search/search.php'
 	),
 );
