f23550c6bd873e3eab01f83a1164fa518c2fed04
retrofox <retrofox@users.noreply.github.com>
2022-04-24 17:19:00 +0000

    JS Components: Update Alert level colors via ThemeProvider (#24062)
    * js-components: Add a couple of yellow and blue colors
    
    * use new colores provided to set the Alter levels
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2216251583

--- jetpack_vendor/automattic/jetpack-my-jetpack/build/index.asset.php
+++ jetpack_vendor/automattic/jetpack-my-jetpack/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '3bc3d2e80d76c5e17b5e19e167bcdcb1');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '57b097c832e2e3c79c52623a7384b151');
