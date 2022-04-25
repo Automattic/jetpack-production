50b6b00a98aa962ca4ad274e1bfeb5551ef504c8
anomiex <anomiex@users.noreply.github.com>
2022-04-25 15:56:44 +0000

    Update dependency @automattic/format-currency to v1.0.1 (#24073)
    * Update dependency @automattic/format-currency to v1.0.1
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2221279704

--- _inc/blocks/donations/view.asset.php
+++ _inc/blocks/donations/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-dom-ready', 'wp-keycodes', 'wp-polyfill', 'wp-url'), 'version' => 'dbc853e01192a661a1f7df06fc3bcd03');
+<?php return array('dependencies' => array('lodash', 'wp-dom-ready', 'wp-keycodes', 'wp-polyfill', 'wp-url'), 'version' => '15ef6a06dac3ee345edbba2420079a35');
--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '03055067cbc0d4059de293b0e75c6d11');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '21b629376c8534f31bce6d9f3a02228e');
