7c553578db0a4ac0bfc779a4575308f4e7c35fcd
mreishus <mreishus@users.noreply.github.com>
2022-04-29 19:33:50 +0000

    pro-status/index.jsx: Remove unused code and simplify (#24158)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2247311078

--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '47fbad8afda713d9759e140095080b71');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'b88b298f81c8ae83336a6b2e1bc85ffa');
