8bf3958bdc267798c0272994bb25bca65b19c664
coder-karen <coder-karen@users.noreply.github.com>
2022-04-29 09:48:44 +0000

    Assistant: Show product price if no intro offer (#24149)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2244549812

--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'defe86c29656fc495bb254f3f086c633');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '72055a10e61bbd52b79b9a35d7a16aec');
