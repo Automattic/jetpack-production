cd6714e6b98a742d5c2ddbceabbb64be5785b764
annacmc <annacmc@users.noreply.github.com>
2022-05-10 20:20:45 +0000

    Search: Record Meter Design Updates (#24225)
    Co-authored-by: sdixon194 <steve.dixon@automattic.com>
    Co-authored-by: Jason Moon <4044428+jsnmoon@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2303061226

--- jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
+++ jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'c87ee91dcd3b9c8bd14dfeb938c2e2aa');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'dfd9516b7a50a23abbda72f4cc342ca2');
