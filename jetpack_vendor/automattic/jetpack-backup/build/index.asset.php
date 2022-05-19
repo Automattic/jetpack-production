9ca6eb876a7c6521a6caac19750aa6e6a1223c5a
kraftbj <kraftbj@users.noreply.github.com>
2022-05-19 21:37:53 +0000

    Fix backup build (#24442)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2354685734

---
+++ jetpack_vendor/automattic/jetpack-backup/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'cc4ea3ec62f3df6a58ff5eeb1f687fa7');
