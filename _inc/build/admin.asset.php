ad950d431531d8f8d2a80561a33e0fa2433857e9
monsieur-z <monsieur-z@users.noreply.github.com>
2022-05-27 14:38:18 +0000

    Fix incorrect product prices (#24461)
    * Jetpack: Correct prices in product descriptions
    
    * changelog
    
    * Update tests
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2397056907

--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'bcee505204b364d282fb2b700d81625f');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '8ece4b2eea43e1d12fa7095736691621');
