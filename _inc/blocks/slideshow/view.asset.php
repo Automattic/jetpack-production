c3bfd66333d25f5639400c5f69026ef53591d7fe
anomiex <anomiex@users.noreply.github.com>
2022-06-13 21:09:34 +0000

    Update dependency postcss to v8.4.14 (#24728)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2491112226

--- _inc/blocks/slideshow/view.asset.php
+++ _inc/blocks/slideshow/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-dom-ready', 'wp-escape-html', 'wp-polyfill'), 'version' => 'fef1336ba0e706059b84');
+<?php return array('dependencies' => array('lodash', 'wp-dom-ready', 'wp-escape-html', 'wp-polyfill'), 'version' => '352e6650c20783b2fc80');
