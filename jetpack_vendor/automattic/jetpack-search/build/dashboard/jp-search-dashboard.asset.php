9e25f0dabed25e305cce7d1746449cb91f645e17
annacmc <annacmc@users.noreply.github.com>
2022-06-14 03:04:01 +0000

    Search: Record Meter. Sort records before passing to RecordMeterBar component (#24731)
    Co-authored-by: Chris R <chris.rosser@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2492478658

--- jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
+++ jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '8f130a3fa41d09fef4d3');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '773bdc4b16cfd248356e');
