c3e9fad749b5eb48cbda28e83ad9642821438520
annacmc <annacmc@users.noreply.github.com>
2022-06-15 04:01:57 +0000

    Search: Separate record meter styles by component (#24732)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2499626539

--- jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
+++ jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '773bdc4b16cfd248356e');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '7bbdc86912f9ad424ac3');
