3006af2eb222eb79be4b9d15c608cc8f61356bd1
anomiex <anomiex@users.noreply.github.com>
2022-06-21 16:47:49 +0000

    Update dependency @automattic/components to v2 (#24794)
    * Update dependency @automattic/components to v2
    * Update pnpmfile, remove unneeded deps from the new version.
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2536891913

--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'cf378e0e9955d27fa77a');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'fa95880176495ec9d6bb');
