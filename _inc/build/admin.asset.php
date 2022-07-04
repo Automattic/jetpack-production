2e78685575f10602f938b469a23494cde93176bf
coder-karen <coder-karen@users.noreply.github.com>
2022-07-04 08:56:34 +0000

    Stats: Allow custom user role stats settings to be recognized (#24887)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2608843310

--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '49d4c425e31d217ae5ec');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'df490db3c9924df1929b');
