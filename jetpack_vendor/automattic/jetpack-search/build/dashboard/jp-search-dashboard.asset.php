f2e306ff4b80096d0bd6347335e6275e04ffe890
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2022-07-13 11:13:05 +0000

    RNA Gridicon: Change `title` to `desc` (#25081)
    * RNA Gridicon: Change title to desc
    
    * changelog
    
    * Fix version
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2662903257

--- jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
+++ jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '4f8a5f0e2e153016d343');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '6be0a5d2f089a9d326ea');
--- jetpack_vendor/automattic/jetpack-wordads/build/dashboard/jp-wordads-dashboard.asset.php
+++ jetpack_vendor/automattic/jetpack-wordads/build/dashboard/jp-wordads-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '9d47eb9e6af59ce02979');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'ba9681c13ddd53a53a17');
