bceb37b4ac6bff58b60e3c56632798bc9fedb414
jeherve <jeherve@users.noreply.github.com>
2022-07-19 11:18:03 +0000

    Repo Gardening: move utilities into their own directory (#25115)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2697150264

--- vendor/automattic/jetpack-plans/vendor/autoload.php
+++ vendor/automattic/jetpack-plans/vendor/autoload.php
@@ -9,4 +9,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit514054480c3d21fd5d0aa81ee118526d::getLoader();
+return ComposerAutoloaderInitaee8e68d6908bd4b0b2441fb6c649e2b::getLoader();
--- vendor/automattic/jetpack-plans/vendor/composer/autoload_static.php
+++ vendor/automattic/jetpack-plans/vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit514054480c3d21fd5d0aa81ee118526d
+class ComposerStaticInitaee8e68d6908bd4b0b2441fb6c649e2b
 {
     public static $files = array (
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
@@ -844,9 +844,9 @@ class ComposerStaticInit514054480c3d21fd5d0aa81ee118526d
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit514054480c3d21fd5d0aa81ee118526d::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit514054480c3d21fd5d0aa81ee118526d::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit514054480c3d21fd5d0aa81ee118526d::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitaee8e68d6908bd4b0b2441fb6c649e2b::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitaee8e68d6908bd4b0b2441fb6c649e2b::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInitaee8e68d6908bd4b0b2441fb6c649e2b::$classMap;
 
         }, null, ClassLoader::class);
     }
