6aa9ff0205c859886ab541d10984a5aefad2f5a3
adimoldovan <adimoldovan@users.noreply.github.com>
2022-07-19 09:25:54 +0000

    E2E tests: don't throw if isElementVisible matches more elements (#25124)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2696471822

--- vendor/automattic/jetpack-plans/vendor/autoload.php
+++ vendor/automattic/jetpack-plans/vendor/autoload.php
@@ -9,4 +9,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInite7f9bb30efee2f5396a0b7e3dec2ddf9::getLoader();
+return ComposerAutoloaderInit514054480c3d21fd5d0aa81ee118526d::getLoader();
