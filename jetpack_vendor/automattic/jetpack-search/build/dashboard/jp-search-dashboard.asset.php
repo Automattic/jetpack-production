649549e806e025f42df9138cecf95a1da17e5311
kangzj <kangzj@users.noreply.github.com>
2022-07-21 09:50:39 +0000

    Search: Add Search Dashboard support for WPCOM simple sites (#25094)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2710782692

--- jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
+++ jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '86e1d103001876e393a1');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '806d2f094934903bce6f');
