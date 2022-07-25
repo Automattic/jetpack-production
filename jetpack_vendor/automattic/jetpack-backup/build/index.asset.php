4353bb07a0000d9590bc3c1343bf43cc0e9b3ac4
oskosk <oskosk@users.noreply.github.com>
2022-07-25 14:22:43 +0000

    [Package]Backup: Add ThemeProvider wrapper to Admin component (#25263)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2732911614

--- jetpack_vendor/automattic/jetpack-backup/build/index.asset.php
+++ jetpack_vendor/automattic/jetpack-backup/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '09fba1f155251280c5fe');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '5e194a1844bd3b0eaa14');
