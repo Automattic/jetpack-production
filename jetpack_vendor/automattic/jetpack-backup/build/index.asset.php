050f70857efc306a272c979b0c10d5fe52ba10ea
oskosk <oskosk@users.noreply.github.com>
2022-07-26 12:39:44 +0000

    [Package] Jetpack Backup: add review request component (#25161)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2739308633

--- jetpack_vendor/automattic/jetpack-backup/build/index.asset.php
+++ jetpack_vendor/automattic/jetpack-backup/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '5e194a1844bd3b0eaa14');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '81de7c9c7fd5a4aa567d');
