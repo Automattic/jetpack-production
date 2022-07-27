e89ecf159b78da3bb7252369897f839b13777bb0
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2022-07-27 11:57:22 +0000

    JS Components: Convert Gridicon component to TypeScript (#25082)
    * Convert Gridicon component to TypeScript
    
    * Convert Gridicon stories to TypeScript
    
    * Add chaneglog
    
    * Fix version
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2746341003

--- jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
+++ jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '7dbe2ba76bb2b6609ed7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '54d51f68f13d5925557e');
--- jetpack_vendor/automattic/jetpack-wordads/build/dashboard/jp-wordads-dashboard.asset.php
+++ jetpack_vendor/automattic/jetpack-wordads/build/dashboard/jp-wordads-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'c03c5212ac908fa59984');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '8cc0cfa1af3e4339c47d');
