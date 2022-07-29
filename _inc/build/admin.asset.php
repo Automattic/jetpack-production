fdcb774c5e0f57de28e2d4a2940a36a1bb14843f
dhasilva <dhasilva@users.noreply.github.com>
2022-07-29 16:34:21 +0000

    JS Components: Allows empty price fraction to be hidden on ProductPrice component (#25318)
    * [not verified] add hidePriceFraction prop to ProductPrice
    
    * [not verified] changelog
    
    * [not verified] bump jetpack components version
    
    * [not verified] adds tests to ProductPrice component
    
    * [not verified] fix render when price is 0
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2761899588

--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '98dd758d5ad24ce0b9ed');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'c444687500c6495f6c5a');
