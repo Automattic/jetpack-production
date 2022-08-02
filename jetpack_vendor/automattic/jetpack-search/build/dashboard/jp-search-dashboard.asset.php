c5c16ce6b69c9ec349897c5bd88bc2ce4664c2cf
bluefuton <bluefuton@users.noreply.github.com>
2022-08-02 02:49:08 +0000

    Record Meter: add info link to docs (#25002)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2779220635

--- jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
+++ jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '0c1e2c5d5cbcbb5e0a07');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '98a917cb9ddb86dc4c2b');
