ed5bd4f00e7c6b303f89266d613a256b48fcd569
nunyvega <nunyvega@users.noreply.github.com>
2022-08-08 21:03:36 +0000

    Backup package: Refactor Admin and ReviewRestore components (#25335)
    * Refactor Admin and ReviewRestore components
    
    * Update class-jetpack-backup.php
    
    * Change button to ExternalLink
    Start opening link in new tab.
    
    * Correct request flicker
    
    Co-authored-by: Samiff <samiff@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2820877835

--- jetpack_vendor/automattic/jetpack-backup/build/index.asset.php
+++ jetpack_vendor/automattic/jetpack-backup/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '2fe715868f58bec306ec');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '69803cf214701854de6a');
