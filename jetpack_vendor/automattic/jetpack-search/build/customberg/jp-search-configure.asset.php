ab346922a5f43ecdbde8ea814c84ce852f3ef91c
a8ck3n <a8ck3n@users.noreply.github.com>
2022-08-11 08:46:58 +0000

    Instant Search: Make display order match tab order on mobile. (#25415)
    Make display order match tab order on mobile. We do this by removing the CSS order attribute on the image. This means the image will be shown below the title instead of above it. For now, we should consider accessibility before styling.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2838467873

--- jetpack_vendor/automattic/jetpack-search/build/customberg/jp-search-configure.asset.php
+++ jetpack_vendor/automattic/jetpack-search/build/customberg/jp-search-configure.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url', 'wp-viewport'), 'version' => '5dd8002391bdc15d5338');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url', 'wp-viewport'), 'version' => 'e9a74534600b73340286');
--- jetpack_vendor/automattic/jetpack-search/build/instant-search/jp-search.asset.php
+++ jetpack_vendor/automattic/jetpack-search/build/instant-search/jp-search.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n', 'wp-jp-i18n-loader', 'wp-polyfill'), 'version' => '6055c793e21e48174125');
+<?php return array('dependencies' => array('wp-i18n', 'wp-jp-i18n-loader', 'wp-polyfill'), 'version' => '4861991df0a120bd6109');
