8d4efebdf9d63b19221d329f441533608f3b9fe3
yashwin <yashwin@users.noreply.github.com>
2022-08-17 04:31:47 +0000

    License Activation Notice: fix issue in license activation notice that appears even after being dismissed (#25269)
    * Fix dismiss user license activation notice
    
    * Add changelog
    
    Co-authored-by: sdixon194 <steve.dixon@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2872716806

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@
 This is an alpha version! The changes listed here are not final.
 
 ### Bug fixes
+- Fix issue in the license activation notice banner that appears even after being dismissed.
 - Remove inclusion modules/videopress/utility-functions.php from Jetpack shortcodes module
 
 ### Other changes <!-- Non-user-facing changes go here. This section will not be copied to readme.txt. -->
--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '582926a13c45d9054da2');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'c72975f86ba59a17bae5');
