0469fa1de7020ace8ee5fff8de31d296e1477933
retrofox <retrofox@users.noreply.github.com>
2022-08-18 12:04:31 +0000

    VideoPress: Initial VideoUploadArea component (#25432)
    * [not verified] add draft VideoUploadArea component
    
    * [not verified] changelog
    
    * [not verified] move VideoUploadArea component to VideoPress package
    
    * [not verified] changelog
    
    * [not verified] apply review suggestions
    
    * [not verified] remove file quantity restriction
    
    * [not verified] add devDependencies to storybook
    
    * move component folder
    
    * add wordpress components dependency
    
    * apply review suggestions
    
    Co-authored-by: Damián Suárez <rdsuarez@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2882191350

--- jetpack_vendor/automattic/jetpack-videopress/CHANGELOG.md
+++ jetpack_vendor/automattic/jetpack-videopress/CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 This is an alpha version! The changes listed here are not final.
 
 ### Added
+- Add the VideoUploadArea component without functionality
 - Migrate VideoPress block to pkg
 - VideoPress: serve a minified token bridge version in production env
 
--- jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
+++ jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '885fb0936327e3939cac');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'a8cf7b745b593c8d33c0');
