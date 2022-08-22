df58947c40da30107a49b4c0fe0ae1f30749afd6
mmtr <mmtr@users.noreply.github.com>
2022-08-22 09:02:10 +0000

    Donation block: Payment modal not displayed on iOS devices (#25721)
    Removed a style that was causing the payment modal to not be shown on iOS devices
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2902463800

--- _inc/blocks/donations/view.asset.php
+++ _inc/blocks/donations/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-dom-ready', 'wp-keycodes', 'wp-polyfill', 'wp-url'), 'version' => 'f0460da6cacf5b741e2d');
+<?php return array('dependencies' => array('lodash', 'wp-dom-ready', 'wp-keycodes', 'wp-polyfill', 'wp-url'), 'version' => '2bc7ec7cd6506a768f8e');
