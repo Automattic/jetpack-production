8598a13f6a6e6e82a582445247740765a402de77
jboland88 <jboland88@users.noreply.github.com>
2022-08-22 19:23:09 +0000

    Add/boost recommendation (#25431)
    * Add some initial strucure for a boost recommendation
    
    * Additional setup for boost recommendations
    
    * Update boost recommendation logic
    
    * changelog
    
    * Add missing condition in switch statement
    
    * Address feedback, fix list output
    
    * Fix small react error
    
    Co-authored-by: Brandon Kraft <public@brandonkraft.com>
    Co-authored-by: sdixon194 <steve.dixon@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2906422111

--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'c72975f86ba59a17bae5');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '6f72620af8eb2abc8b7a');
