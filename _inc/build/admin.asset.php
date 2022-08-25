226941a01bee5e1f2097ddcd08d6adb37f6d2358
anomiex <anomiex@users.noreply.github.com>
2022-08-25 20:51:11 +0000

    Update dependency @automattic/popup-monitor to v1.0.2 (#25845)
    * Update dependency @automattic/popup-monitor to v1.0.2
    * Remove obsoleted pnpmfile override
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2929524799

--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'dfa5113907f2392cf357');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'b6685a1ba2b3c5a02392');
