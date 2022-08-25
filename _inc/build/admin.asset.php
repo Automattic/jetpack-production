391e3edd7a7be6357c488f566546cf4245ca53cc
anomiex <anomiex@users.noreply.github.com>
2022-08-25 21:32:32 +0000

    Update dependency @automattic/social-previews to v1.1.5 (#25846)
    * Update dependency @automattic/social-previews to v1.1.5
    * Remove obsoleted pnpmfile override
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/2929739835

--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'b6685a1ba2b3c5a02392');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'ab6c5cd34bdd9ee1d547');
