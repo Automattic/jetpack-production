d6bc92d538d0e178d56c366d860e45649d54b3cc
retrofox <retrofox@users.noreply.github.com>
2022-09-06 22:20:00 +0000

    VideoPress: Update `AdminPage` to alpha version using mocks (#25905)
    * VideoPress: Support enable and disable columns and actions
    
    * changelog
    
    * VideoPress: Improve experience at small screens
    
    * VideoPress: Better check for plays and duration
    
    * [not verified] VideoPress: Initial AdminPage
    
    * VideoPress: Fix types and support hidePrivacy
    
    * VideoPress: Hide duration
    
    * VideoPress: Hide plays
    
    * VideoPress: Fix checkbox styles inside wpadmin
    
    * VideoPress: Hide edit button
    
    * changelog
    
    * VideoPress: Update mock place
    
    * VideoPress: Fixes into input
    
    * VideoPress: Extract library to own component
    
    * VideoPress: Map types across places
    
    * VideoPress: Change from videoTitle to title
    
    * VideoPress: Map original videos
    
    * VideoPress: Implement grid toggle
    
    * VidePress: Hide filter in local library
    
    Co-authored-by: retrofox <rdsuarez@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3003677217

--- jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
+++ jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '222eb61e052dc800044a');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '5cc57a5e3a8085cc5c1e');
