d0cbfd37a68b1907d73eb1c88ef9b990790db690
renatoagds <renatoagds@users.noreply.github.com>
2022-09-08 18:41:48 +0000

    VideoPress: Remove blank spaces from `Logo` (#26121)
    * [not verified] VideoPress: Fix white space into VideoPress logo
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3017364672

--- jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
+++ jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '249edb34750f9a629bf4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '889f700f4740b62da8cf');
