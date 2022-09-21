0786dfe403083504e54167a155f3c93ec141986d
kraftbj <kraftbj@users.noreply.github.com>
2022-09-21 21:18:34 +0000

    Backup plugin: hide Cloud links to disconnected users (#26327)
    * Remove links to Jetpack Cloud for disconnected users
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3101075755

--- jetpack_vendor/automattic/jetpack-backup/build/index.asset.php
+++ jetpack_vendor/automattic/jetpack-backup/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '56448891b21a7a20bbc8');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '1861580476213c80df58');
