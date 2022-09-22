acd327fd02313aa8e600a92f3ab15fe8280d2ff0
oskosk <oskosk@users.noreply.github.com>
2022-09-22 18:31:12 +0000

    VideoPress: fix error on undefined preview image (#26356)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3107747103

--- jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
+++ jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '3581adc95a2cf3f8769c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '89fd5b99fc1309761086');
