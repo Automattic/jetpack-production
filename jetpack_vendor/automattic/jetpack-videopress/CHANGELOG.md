c0b2d8387cb8cac87d7090ac7a5d5aa8b4d77338
dhasilva <dhasilva@users.noreply.github.com>
2022-09-22 21:20:37 +0000

    VideoPress: Fix video counter on VideoPress library page (#26365)
    * Support videosOnPage argument on VideoLibraryWrapper component
    
    * Add optional totalVideos prop on VideoLibrary components
    
    * Use new totalVideos prop on the VideoPressLibrary component
    
    The VideoPressLibrary component now expects the new totalVideos prop, and uses it while rendering the VideoLibraryWrapper component.
    
    It also uses the `videos` array length as the number of videos available on the current page.
    
    * Set totalVideos prop on the Video Library components
    
    * Use new totalVideos prop on the LocalLibrary component
    
    * Remove unnecessary videosOnPage prop
    
    The videosOnPage counter is not necessary, based on the Figma designs for this page.
    
    * Set proper plural and i18n on the video counter
    
    * Include changelog file
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3108750552

--- jetpack_vendor/automattic/jetpack-videopress/CHANGELOG.md
+++ jetpack_vendor/automattic/jetpack-videopress/CHANGELOG.md
@@ -20,6 +20,7 @@ This is an alpha version! The changes listed here are not final.
 - VideoPress: resolve addVideo() selector
 
 ### Fixed
+- Set the right source for the total of videos counter on the VideoPress library page.
 - VideoPress: Fix SearchInput clear and initial typing behavior on dashboard
 - VideoPress: fix updating the store when editing video data
 
--- jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
+++ jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'dc3bbeb63f503a059f90');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '15d9a8af195c7710bbc5');
