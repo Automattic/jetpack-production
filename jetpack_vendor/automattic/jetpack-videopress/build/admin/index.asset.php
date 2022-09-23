0b58eff54d5a007eb7d19fc7cc2db99f7590fa8a
retrofox <retrofox@users.noreply.github.com>
2022-09-23 17:03:02 +0000

    VideoPress: add updateVideoPrivacy() action (#26380)
    * add updateVideoPrivacy() action
    
    * check valid privacy level before to hit the server-side
    
    * changelog
    
    * tidy
    
    * reduce properly when setting partially a video item
    
    Co-authored-by: Douglas Henri <dhasilva@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3114276444

--- jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
+++ jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'f83971e7caff4cfd1d84');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '1bb8cf04e04f7608e77e');
