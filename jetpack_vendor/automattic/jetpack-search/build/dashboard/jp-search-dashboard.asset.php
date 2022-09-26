e630dc779a72975bf9c4e76e461114f2887cac15
nunyvega <nunyvega@users.noreply.github.com>
2022-09-26 15:40:11 +0000

    Search: Refactor renderMockedSearchInterface to be a component (#26437)
    * [not verified] Refactor renderMockedSearchInterface to be a component
    
    * Add changelog
    
    Co-authored-by: oskosk <oskosk@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3128934647

--- jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
+++ jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '36cad5f1e1c1ce4e139e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'f59bb4b40b6ce414dc56');
