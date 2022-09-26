1e80a3ff8f5782b5112b1f9c9d90ab9776494572
nunyvega <nunyvega@users.noreply.github.com>
2022-09-26 12:58:54 +0000

    Search: change connectScreen so it is a component instead of the renderConnectScreen component (#26430)
    * [not verified] Refactor renderSearchResult to be a component
    
    * changelog
    
    * [not verified] Refactor renderInstantSearchToggle to be a component
    
    * Changelog
    
    * [not verified] Refactor renderConnectScreen to be a component
    
    * Changelog
    
    Co-authored-by: oskosk <oskosk@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3127851395

--- jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
+++ jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'bcf50299d9fffb6cf60e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'a902aa3739755c84e0dd');
