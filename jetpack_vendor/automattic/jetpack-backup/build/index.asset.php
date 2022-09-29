0a3d45087649d6f02c830706977956057d175f80
oskosk <oskosk@users.noreply.github.com>
2022-09-29 14:39:28 +0000

    Add/headline to backup upgrade view (#26527)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3152055047

--- jetpack_vendor/automattic/jetpack-backup/build/index.asset.php
+++ jetpack_vendor/automattic/jetpack-backup/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'e7b37e688e848b618f6e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '4a5251df1e560db2013d');
