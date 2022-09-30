033cb3f9a92ff167b3db9722cabd3f4422d5ab2b
SiobhyB <SiobhyB@users.noreply.github.com>
2022-09-30 15:16:33 +0000

    [RNMobile] Update Get Apps card to reflect the Jetpack app (#26276)
    The following changes have been made to the "get apps" section found via Jetpack → Dashboard:
    
    - The section is no longer dismissable.
    - A QR code is available for users to scan when viewed on desktop, with buttons on mobile.
    - Some updates have been made to the design, including tweaks to padding, the new app icon, etc.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3159706280

--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '777017568beb2572963d');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'fdeef0d2cd2a3261f967');
