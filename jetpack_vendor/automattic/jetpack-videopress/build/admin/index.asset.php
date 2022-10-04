d986401b3ac8dd8b81a436e13f28d8c01fb90f6b
oskosk <oskosk@users.noreply.github.com>
2022-10-04 21:46:02 +0000

    VideoPress: Fix connection for VideoPress free (#26623)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3185607644

--- jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
+++ jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'b0161cfe9bc5fa63e3cf');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'd400c7325755e739b989');
