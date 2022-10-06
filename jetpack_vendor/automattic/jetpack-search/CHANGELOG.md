a6b69188f3af5c2c5265db4fbcfce86f54841952
jsnmoon <jsnmoon@users.noreply.github.com>
2022-10-06 19:26:47 +0000

    Search Dashboard: Conditional CUTs. (#26656)
    * Search Dashboard: Conditional CUTs.
    
    Adds the localized upgrade triggers and a component for displaying one in the Plan Usage section.
    
    * Cleaner setup in CUTWrapper component.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3199742097

--- jetpack_vendor/automattic/jetpack-search/CHANGELOG.md
+++ jetpack_vendor/automattic/jetpack-search/CHANGELOG.md
@@ -9,6 +9,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 This is an alpha version! The changes listed here are not final.
 
+### Added
+- Search Dashboard: Add support for conditional CUTs.
+
 ### Changed
 - Apply ContextualUpgradeTrigger with checkout CTA
 
--- jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
+++ jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'e877625041ad3b8a31ac');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'fb7ce95ef6ebb7c8e624');
