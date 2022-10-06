8a827d4732901b6c0768cd61ad5f9a8b7ecf4e50
pablinos <pablinos@users.noreply.github.com>
2022-10-06 08:27:22 +0000

    Social: Prevent upgrade links from opening in a new tab (#26649)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3195587429

--- jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
+++ jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'b07292af90d1ed48b127');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '5a2396e4c1779312f6eb');
