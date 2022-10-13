75ecb4f98af955dc8d099367e41d890a34156666
sergeymitr <sergeymitr@users.noreply.github.com>
2022-10-13 12:16:01 +0000

    Search: add connection error notice (#26778)
    Add connection error notice to the Search plugin
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3242161145

--- jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
+++ jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'e20eb83df7a7f3913150');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'b92d074793b564b7cd0b');
