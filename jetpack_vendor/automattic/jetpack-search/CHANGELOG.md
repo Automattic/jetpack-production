e92b23f2a40e92143efe43ab5c1886a8b34d8f11
kangzj <kangzj@users.noreply.github.com>
2022-10-18 21:28:16 +0000

    Search: Use API values for paid allowance limits shown in the new pricing table (#26895)
    * Use API values for paid allowance limits
    
    * changelog
    
    * Add missing space
    
    * Improve unlimited requests handling
    
    * Wire up unit pricing values from API
    
    * Wire up unit pricing values from API
    
    * Bump versions
    
    * Calculate discount percentage
    
    * Only use offPrice is discount is perceivable
    
    * Move discount calculation into selector
    
    * Introduce fallback when prices aren't discounted
    
    * Use selector when possible
    
    * Use monthly value for unit price
    
    * Fix selector errors
    
    * Properly show monthly unit price
    
    * Grammar!
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3276955044

--- jetpack_vendor/automattic/jetpack-search/CHANGELOG.md
+++ jetpack_vendor/automattic/jetpack-search/CHANGELOG.md
@@ -14,6 +14,7 @@ This is an alpha version! The changes listed here are not final.
 
 ### Changed
 - Updated package dependencies.
+- Use API values for paid allowance limits shown in the new pricing table
 
 ## [0.28.0] - 2022-10-17
 ### Changed
--- jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
+++ jetpack_vendor/automattic/jetpack-search/build/dashboard/jp-search-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'e9ceb7d786dac788fa47');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '28f21f4d84367537e26a');
