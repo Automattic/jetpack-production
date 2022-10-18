309991bd43304038fa663d9355f8ccf10140bde9
dhasilva <dhasilva@users.noreply.github.com>
2022-10-18 19:16:33 +0000

    VideoPress: Add confirmation before leaving page when upload is in progress (#26912)
    * add useUploadUnloadCheck custom hook
    
    * use useUploadUnloadCheck hook on main component
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3276169601

--- jetpack_vendor/automattic/jetpack-videopress/CHANGELOG.md
+++ jetpack_vendor/automattic/jetpack-videopress/CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 This is an alpha version! The changes listed here are not final.
 
 ### Added
+- VideoPress: Add confirmation before leaving page when upload is in progress
 - VideoPress: track page view and checking out events
 
 ### Changed
--- jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
+++ jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '35432c6d2cc515299dd3');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'b26c1bbaaa8711da2b3d');
