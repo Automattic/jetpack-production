37bd476af9389e2e95b810d0b1058e485dc1a320
renatoagds <renatoagds@users.noreply.github.com>
2022-10-20 21:50:23 +0000

    VideoPress: Upload video from local library (#26948)
    * VideoPress: Remove ConnectLocalLibrary
    
    * VideoPress: Remove extra prop from VideoThumbnail
    
    * VideoPress: Move prop to showActionButton
    
    * VideoPress: Allow customization of action button label
    
    * VideoPress: Use actionClick in VideoRow
    
    * VideoPress: Use Upload to VideoPress as button label in local video list
    
    * VideoPress: Extract uploadFromLibrary out of block
    
    * VideoPress: Look for upload_src and resolve and catch on recall
    
    * VideoPress: Implement uploadVideoFromLibrary action
    
    * changelog
    
    * VideoPress: Set result as data in handleUploadSuccess
    
    * VideoPress: Keep fetching state when new or resume status from upload
    
    * VideoPress: Cast media id to int in uploader from library
    
    * VideoPress: Use temp id at upload from library
    
    * VideoPress: Return isUploadedToVideoPress field in local data
    
    * VideoPress: Disable hover when is already uploaded to VideoPress
    
    * VideoPress: Rename hoverable to disabled
    
    * VideoPress: Support titleAdornment in VideoRow
    
    * VideoPress: Use adornment if uploaded to VideoPress
    
    * VideoPress: Disable checkbox
    
    * VideoPress: Fix local videos count
    
    * VideoPress: Mark local video as uploaded
    
    * VideoPress: Update showActions logic
    
    * VideoPress: Improve feedback while uploading
    
    Co-authored-by: sdixon194 <steve.dixon@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3293099234

--- jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
+++ jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '82773b9bd1b9fffcfb32');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '2f9dce49b8f052205927');
