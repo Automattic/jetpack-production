a39d096d9fd3fc8b62bef58e45f6a1e4f8a77f5b
retrofox <retrofox@users.noreply.github.com>
2022-10-23 23:46:02 +0000

    VideoPress: Add check for empty video poster on polling after update (#27000)
    * check for empty poster image on video polling after upload
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3308999590

--- jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
+++ jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '3baed8fefbf0385e1c72');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'b5701a98e65c452dd7aa');
