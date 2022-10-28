8ccc523f9f1d9350041e92b532b36f16e3e7c310
retrofox <retrofox@users.noreply.github.com>
2022-10-28 15:55:00 +0000

    VideoPress: add link to go back from video details page (#27132)
    * Add go back link to video details view
    
    * changelog
    
    * Update projects/packages/videopress/src/client/admin/components/edit-video-details/style.module.scss
    
    Co-authored-by: Damián Suárez <rdsuarez@gmail.com>
    
    Co-authored-by: Damián Suárez <rdsuarez@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3346676032

--- jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
+++ jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'a37398e52af5930e40c2');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '2990d9089b3fdeb3f150');
