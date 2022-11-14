e36f4a77369ae13a7f221f50af0eebdbf5172e7a
dhasilva <dhasilva@users.noreply.github.com>
2022-11-14 13:01:35 +0000

    VideoPress: Allow keyboard navigation on video quick actions (#27378)
    * make video card actions tabbable
    
    * remove showActionState in favor of CSS on VideoRow
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3461572260

--- jetpack_vendor/automattic/jetpack-videopress/CHANGELOG.md
+++ jetpack_vendor/automattic/jetpack-videopress/CHANGELOG.md
@@ -21,6 +21,7 @@ This is an alpha version! The changes listed here are not final.
 
 ### Changed
 - Updated package dependencies.
+- VideoPress: Allow keyboard navigation on video quick actions
 - VideoPress: Allow selection of multiple files
 - VideoPress: introduce PlaybackControl component
 - VideoPress: remove the fallback video
--- jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
+++ jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '320c7b35892b764bcf37');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '735f3a2df72a0275d90a');
