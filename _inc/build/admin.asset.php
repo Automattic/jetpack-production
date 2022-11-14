b6832b049a17a78755a74b556403647ce72efeb3
coder-karen <coder-karen@users.noreply.github.com>
2022-11-14 11:24:35 +0000

    Dashboard: Remove the user license activation notice (#27329)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3460892289

--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '7dfbc4c2bf895eb4f81f');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'fba34e0501815d8e50d4');
