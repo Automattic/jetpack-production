aeafd3d7c99675a5c87cd9a6afb8df1f37bfb4db
nunyvega <nunyvega@users.noreply.github.com>
2022-11-22 13:36:04 +0000

    MyJetpack: Add Manage Connection Dialog (#27476)
    * [not verified] Add manageConnectionDialog component to My Jetpack
    
    * [not verified] changelog
    
    * [not verified] Update tests
    
    * Center action card headline vertically
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3523763071

--- jetpack_vendor/automattic/jetpack-my-jetpack/build/index.asset.php
+++ jetpack_vendor/automattic/jetpack-my-jetpack/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'c8b0206144b22212a975');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '685edc89f1bee6690864');
