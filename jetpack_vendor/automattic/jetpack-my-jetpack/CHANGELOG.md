7c5817ab1d8e7b946b9f1fd6acb9d29fc9c37d46
oskosk <oskosk@users.noreply.github.com>
2022-12-02 16:32:07 +0000

    My Jetpack: Requires/Push to connection only if needed (#27615)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3603319070

--- jetpack_vendor/automattic/jetpack-my-jetpack/CHANGELOG.md
+++ jetpack_vendor/automattic/jetpack-my-jetpack/CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 This is an alpha version! The changes listed here are not final.
 
 ### Changed
+- My Jetpack: Requires connection only if needed
 - Updated package dependencies.
 
 ## [2.5.1] - 2022-11-30
--- jetpack_vendor/automattic/jetpack-my-jetpack/build/index.asset.php
+++ jetpack_vendor/automattic/jetpack-my-jetpack/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '9f082dd71c0befac2d36');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'ca6a98f880ad73e3a364');
