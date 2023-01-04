1a4c746ebfcd7657af556288d2f43997d7bf737d
jkguidaven <jkguidaven@users.noreply.github.com>
2023-01-04 05:21:18 +0000

    Licensing: Fix issue in activate license key button not working. (#28014)
    * Remove useEffect in ActivationScreen component that sets the JP Rest API root and nonce.
    
    * changelog
    
    * Bump Licensing package version.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/3835365024

--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '192359540340e8ef8a4b');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'a726d4896910bf7ad814');
