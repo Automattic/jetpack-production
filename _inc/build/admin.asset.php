b3cd5ae0bda4d1445f319aff892eecf3bcd64ac4
nunyvega <nunyvega@users.noreply.github.com>
2023-02-02 11:54:31 +0000

    Jetpack dashboard navigation: add external link icon to Plans link (#28500)
    * [not verified] Add external link icon to Plans navigation item
    
    * [not verified] changelog
    
    * [not verified] Add fill matching link color to icon
    
    * [not verified] use ExternalLink component
    
    * [not verified] Correct styling
    
    * [not verified] Modify styling to properly display in smaller screens
    
    * Update tests
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4074086771

--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'a68ee071ab92898cc1b4');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '98679c219f29d5acd228');
