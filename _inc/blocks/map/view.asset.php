42598579d6ab566f79cfdd4bc2096cbfe1f5ab89
anomiex <anomiex@users.noreply.github.com>
2023-02-08 19:43:57 +0000

    build: Add more debug statements (#28850)
    Problem seems to happen somewhere during the copying the project files.
    Most likely at the end, since it seems like all files were in fact
    copied. So add logging in that bit.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4127678740

--- _inc/blocks/map/view.asset.php
+++ _inc/blocks/map/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-polyfill', 'wp-token-list'), 'version' => 'ef8f928c96dc5fb9bf8c');
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-polyfill'), 'version' => 'ff7359d930ccf62cec46');
