80a8a20142ba8507efd6c68733c489090a483881
anomiex <anomiex@users.noreply.github.com>
2023-02-21 14:22:52 +0000

    Update minimum pnpm version (#29053)
    Picking up a few useful changes in this update:
    * v7.20 brings a native implementation of `pnpm config`, which will be
      needed when our docker container gets updated to node 18.14.
    * v7.24 brings an improved lockfile format, and the end of mystery
      hashes in the lockfile and virtual store.
      * v7.26.3 brings a fix to that for storybook.
    * v7.26.0 brings `pnpm dedupe`, which seems likely to be useful for
      renovate.
    
    This PR changes the lockfile to a new format! If you have merge
    conflicts after this is merged, the best thing to do is likely to
    `git checkout origin/trunk pnpm-lock.yaml && pnpm install` to regenerate
    it instead of trying to resolve the conflicts by hand.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4233459561

--- _inc/blocks/slideshow/view.asset.php
+++ _inc/blocks/slideshow/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready', 'wp-escape-html', 'wp-i18n', 'wp-polyfill'), 'version' => '88120f2783c9b32b7c24');
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-escape-html', 'wp-i18n', 'wp-polyfill'), 'version' => '99a5b3cbb8c4f9d9802a');
