a35178c9597af377a3e09fb7d964424fd5c092cb
ice9js <ice9js@users.noreply.github.com>
2023-03-07 15:14:26 +0000

    Implement RWD navigation for feedback dashboard (#29315)
    * [not verified] Fix RWD styles for the table and response
    
    * [not verified] Add navigation for switching between inbox and response views on mobile
    
    * Update layout component to accept custom class names
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4355448685

--- jetpack_vendor/automattic/jetpack-forms/dist/dashboard/jetpack-forms-dashboard.asset.php
+++ jetpack_vendor/automattic/jetpack-forms/dist/dashboard/jetpack-forms-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '4f4732cf10732f936f92');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '86f92acb59f41715a39e');
