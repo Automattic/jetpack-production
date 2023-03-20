74bc2fa655623dec2c33e9d9bc735169b6f08bbc
sergeymitr <sergeymitr@users.noreply.github.com>
2023-03-20 12:47:58 +0000

    Connection: switch the "plugin activated" banner to the common connection flow (#29526)
    - The connection banner that appears after plugin activation relies on the old connection flow, and sends API requests itself. We'll switch it to using the new "Connection Screen" flow.
    - Make the Connection Screen utilize the `from` parameter, and pass it further to Calypso.
    - Remove a bunch of legacy code that's no longer needed.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4468198809

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -16,6 +16,7 @@ This is an alpha version! The changes listed here are not final.
 - Sites API: add was_ecommerce_trial flag to data returned for site details
 
 ### Improved compatibility
+- Connection: switch the "plugin activated" banner to the common connection flow.
 - Pinterest Block: ensure the block does not trigger errors when using WordPress 6.2.
 
 ### Bug fixes
--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '61582bc281dff29335db');
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'ad1de2f64719f36a8d0b');
