52bc6a81f3c3821503622dc89c0cf90db640071f
CGastrell <CGastrell@users.noreply.github.com>
2023-03-21 16:35:48 +0000

    Add search input for responses inbox and styles (#29397)
    * add search input component and styles. include app wide styles loaded at root
    
    * use new search input component
    
    * cleanup and fix gap between action row items
    
    * changelog
    
    * make endAdornment go invisible instead of null, so input doesn't change width when typing
    
    * use style vars when possible, remove padding from input element
    
    * use button.secondary on dropdown menun
    
    * rename class for consistency
    
    * override dropdown toggle styles, use vars when possible
    
    * use white instead of inverted
    
    * use root style file to define more vars. then overwrite wp-theme with current defined vars. cyclic yet effective
    
    * use a wider search input
    
    * Code clean up
    
    * Fix loader icon
    
    ---------
    
    Co-authored-by: João Heringer <joao.heringer@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4481345601

--- jetpack_vendor/automattic/jetpack-forms/CHANGELOG.md
+++ jetpack_vendor/automattic/jetpack-forms/CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 This is an alpha version! The changes listed here are not final.
 
 ### Changed
+- Add search input and styles
 - Jetpack Forms: changed "message sent" tracking from Tracks to bump stat.
 
 ## [0.9.0] - 2023-03-20
--- jetpack_vendor/automattic/jetpack-forms/dist/dashboard/jetpack-forms-dashboard.asset.php
+++ jetpack_vendor/automattic/jetpack-forms/dist/dashboard/jetpack-forms-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e3cc8374f15ec6de6fc7');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b6c7a8ae996dceed95ed');
