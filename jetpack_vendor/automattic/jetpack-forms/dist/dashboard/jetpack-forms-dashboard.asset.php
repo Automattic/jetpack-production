e57fa3e89e1723417c66d07d301e7716afd5f34d
donnchawp <donnchawp@users.noreply.github.com>
2023-03-27 09:52:37 +0000

    Change/forms inbox records limit (#29701)
    * Show 50 records on the Forms Responses page
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4530909532

--- jetpack_vendor/automattic/jetpack-forms/dist/dashboard/jetpack-forms-dashboard.asset.php
+++ jetpack_vendor/automattic/jetpack-forms/dist/dashboard/jetpack-forms-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '6d21a113f59821e8cde1');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '07dac222dc4291ce5f8b');
