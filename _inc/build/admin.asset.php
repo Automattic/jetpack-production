d4534faa29ba9d68a3824c3affe6abddd970856b
jeherve <jeherve@users.noreply.github.com>
2023-03-30 12:17:33 +0000

    Add context to the "Upgrade" string to differentiate between an upsell and an update (#29667)
    Co-authored-by: sdnunca <sorin@php.net>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4564077569

--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'be6e90b9a8db72a9fa1e');
+<?php return array('dependencies' => array('jquery', 'lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '11d1f1e65bfb0ba15568');
