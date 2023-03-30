1aaf8a56812e871923e6b2662ecea99e89854e10
lhkowalski <lhkowalski@users.noreply.github.com>
2023-03-30 17:29:59 +0000

    My Jetpack: Add VideoPress stats selector (#29764)
    * Add actions to set product stats and fetching state
    
    * Add reducer to update state with product stats and fetching status
    
    * Add selector to read product stats and fetching status data from the state
    
    * Add changelog file
    
    * Rename changelog file to match branch name
    
    * Add resolver for the product stats
    
    Given each product has it's own endpoint and it's own set of relevant stats, the resolver will delegate the resolution of some product stats to a product-specific resolver.
    
    * Update VideoPress stats resolver to get data from the new featured stats endpoint
    
    * Set stats to null when the request fails
    
    This way, the requester can know that it's not available and can rely on the null value.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4567104619

--- jetpack_vendor/automattic/jetpack-my-jetpack/build/index.asset.php
+++ jetpack_vendor/automattic/jetpack-my-jetpack/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '7f40acb455d69f6f8689');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'e53c4b25574962d6c921');
