c7b4e8fa85e0795dd8de13bf255f58940f0c4ada
jeherve <jeherve@users.noreply.github.com>
2023-04-13 13:40:54 +0000

    Recommendations: update links to Jetpack Mobile (#30063)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4689729440

--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '63665aa5f12e6445b2ac');
+<?php return array('dependencies' => array('jquery', 'lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '0a4c401bebd3e4059780');
