31f93b5fba56339b7cc2f34dbb691936ca98da3c
retrofox <retrofox@users.noreply.github.com>
2023-04-19 11:51:19 +0000

    VideoPress: autoplay video also when Preview On Hover is enabled (#30181)
    * do not pause when `autoplay` is enabled
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4742786655

--- jetpack_vendor/automattic/jetpack-videopress/build/block-editor/blocks/video/view.asset.php
+++ jetpack_vendor/automattic/jetpack-videopress/build/block-editor/blocks/video/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready', 'wp-polyfill'), 'version' => 'f283c364b78df6026cbe');
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-polyfill'), 'version' => '001c876097d05e80e84c');
