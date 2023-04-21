240b4bff1c25dd75d1f775f035db6e3b97274e81
renatoagds <renatoagds@users.noreply.github.com>
2023-04-21 19:16:44 +0000

    My Jetpack: Enable menu for all hybrid products (#30247)
    * My Jetpack: Enable menu for all hybrid products
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4767972193

--- jetpack_vendor/automattic/jetpack-my-jetpack/CHANGELOG.md
+++ jetpack_vendor/automattic/jetpack-my-jetpack/CHANGELOG.md
@@ -14,6 +14,7 @@ This is an alpha version! The changes listed here are not final.
 - My Jetpack: Add Install/Activate menu actions based on the standalone plugin status.
 - My Jetpack: Add side-effect action to request the standalone plugin installation on the backend.
 - My Jetpack: Add stats to VideoPress card
+- My Jetpack: Enable menu for all hybrid products
 - My Jetpack: Reload page after standalone action
 
 ### Changed
--- jetpack_vendor/automattic/jetpack-my-jetpack/build/index.asset.php
+++ jetpack_vendor/automattic/jetpack-my-jetpack/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'f4576ad939b12bb6f736');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'b094b0fdf24ea5714ffa');
