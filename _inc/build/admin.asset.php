b99669c10fa16c9f6d81f0bfed66632a33add74e
nateweller <nateweller@users.noreply.github.com>
2023-05-01 21:14:46 +0000

    RNA: Settings Fix Up (#30347)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4854840504

--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '8ed29ebd98b49ac075e0');
+<?php return array('dependencies' => array('jquery', 'lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'db3286306366e21d19da');
