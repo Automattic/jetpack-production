9746cc7418938ff1181b6ce640e57bd28abb6ebf
kallehauge <kallehauge@users.noreply.github.com>
2023-05-08 15:57:16 +0000

    Jetpack > My Plan: Show plans without expire date as active (#30506)
    * Jetpack > My Plan: Show lifetime plans/products as active
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4916923928

--- _inc/build/admin.asset.php
+++ _inc/build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'e4284330f1a8e004a0a6');
+<?php return array('dependencies' => array('jquery', 'lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '640f80399b12de7ed98f');
