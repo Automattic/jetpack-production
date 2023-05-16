6be9064514ffccfc843056f945f7f3dcdfd7b1c5
rustamshaikh <rustamshaikh@users.noreply.github.com>
2023-05-16 18:55:09 +0000

    Jetpack Backup: Show the forecast information popover (#30731)
    * Added selector for lastBackupSize
    
    * Added storage usage popover with forecast estimate
    
    * changelog
    
    * Add test cases for lastBackupSize selector and reducer
    
    * Made the popover to be in pre-opened state by default on page load until user dismisses it for atleast once.
    
    * Fixed some popover toggle button styling
    
    * updated changelog
    
    * Package version fix
    
    * - Updated tracks event name
    - Replaced a tag with ExternalLink
    
    * Fixed formatting
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/4995523586

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -18,7 +18,7 @@ return array(
     ),
     'jetpack-backup-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-backup',
-      'ver' => '1.12.16',
+      'ver' => '1.12.17-alpha',
     ),
     'jetpack-blaze' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-blaze',
