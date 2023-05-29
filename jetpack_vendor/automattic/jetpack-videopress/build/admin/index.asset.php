01b445b4224cbf3b37ecaa875c99ee6696be6230
anomiex <anomiex@users.noreply.github.com>
2023-05-29 18:56:57 +0000

    Update pnpm to v8 (#30921)
    Mainly because people have been getting confused when installing the
    latest pnpm and then it doesn't work.
    
    But 8.3.0 does bring a useful new flag to `pnpm install` that we can use
    to have CI better avoid people accidentally committing broken peer
    dependencies.
    
    Also we need a few config changes to maintain expected behavior as v8
    made some questionable changes to the defaults.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5114460559

--- jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
+++ jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'b7080bbc039fb4a5bfad');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '3a2bcdd97fd2036ad981');
