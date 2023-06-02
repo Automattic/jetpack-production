c563ca8bbc8ea8f565a560d3f7cb229e433036d6
nateweller <nateweller@users.noreply.github.com>
2023-06-02 19:39:57 +0000

    My Jetpack: Filter available licenses on #/add-license page (#31088)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5159008220

--- jetpack_vendor/automattic/jetpack-my-jetpack/CHANGELOG.md
+++ jetpack_vendor/automattic/jetpack-my-jetpack/CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 This is an alpha version! The changes listed here are not final.
 
 ### Changed
+- Filter out revoked licenses from the license activation options.
 - Updated package dependencies.
 
 ## [2.14.1] - 2023-05-29
--- jetpack_vendor/automattic/jetpack-my-jetpack/build/index.asset.php
+++ jetpack_vendor/automattic/jetpack-my-jetpack/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'c5d75afc3b80a53f615c');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'f143a500541a8b103aad');
