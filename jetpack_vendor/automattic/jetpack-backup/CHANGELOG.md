b2e1481286c5f9f61a29a783a4611a78770b380b
CodeyGuyDylan <CodeyGuyDylan@users.noreply.github.com>
2023-06-09 18:36:05 +0000

    Add why i need backup section to connect page (#31285)
    * Fix class selectors
    
    * Add type declaration for svg imports
    
    * Add icon assets
    
    * Add why i need vp backup section component
    
    * Add section to connect page
    
    * changelog
    
    * remove style changes
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5224808231

--- jetpack_vendor/automattic/jetpack-backup/CHANGELOG.md
+++ jetpack_vendor/automattic/jetpack-backup/CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 This is an alpha version! The changes listed here are not final.
 
 ### Added
+- Add "Why I need VaultPress Backup" section to connect page
 - Add video section to backup connect page
 
 ## [1.14.0] - 2023-06-06
--- jetpack_vendor/automattic/jetpack-backup/build/index.asset.php
+++ jetpack_vendor/automattic/jetpack-backup/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'd923308d8be34b36502f');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '25b6f9ae68fdd5823131');
