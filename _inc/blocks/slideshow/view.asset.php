ae5461297237b04a00801012eb0873b1708166ca
anomiex <anomiex@users.noreply.github.com>
2023-06-20 22:50:45 +0000

    Lock file maintenance (#31308)
    * Lock file maintenance
    * Downgrade framer-motion to avoid bug
      https://href.li/?https://github.com/framer/motion/issues/2169
    * Downgrade aria-query to avoid bug
      https://href.li/?https://github.com/A11yance/aria-query/issues/512
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/5328006831

--- _inc/blocks/slideshow/view.asset.php
+++ _inc/blocks/slideshow/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready', 'wp-escape-html', 'wp-i18n', 'wp-polyfill'), 'version' => '93daa925ecbd006d6df9');
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-escape-html', 'wp-i18n', 'wp-polyfill'), 'version' => 'fdf341d24ca8fd1c0634');
