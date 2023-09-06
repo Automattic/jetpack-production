35eb2e413dfb49a7c2d88580ddba2ec7a2c6aacb
alshakero <alshakero@users.noreply.github.com>
2023-09-06 13:42:05 +0000

    Setup blogroll attributes and context (#32807)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6097705000

--- _inc/blocks/blogroll/view.asset.php
+++ _inc/blocks/blogroll/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '5843e582ab6a832a1dae');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'f880a03c7c5029349a11');
