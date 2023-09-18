ab229ac93944f9766828966eaf27c5bf944f4252
artpi <artpi@users.noreply.github.com>
2023-09-18 12:08:31 +0000

    AI Chat: Customizable placeholder text (#33087)
    * [not verified] Rebase trunk for AI Chat: Customizable placeholder text.
    
    * [not verified] changelog
    
    ---------
    
    Co-authored-by: artpi <artur.piszek@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6222061670

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -49,6 +49,7 @@ This is an alpha version! The changes listed here are not final.
 
 ### Other changes <!-- Non-user-facing changes go here. This section will not be copied to readme.txt. -->
 - Added to-test.md entries for 12.6.
+- AI Chat: Add customizable placeholder text
 - AI Chat: Basic error handling and minor refactor into components.
 - Fixed video previews for Social Previews
 - Refactor experimental blocks registration
--- _inc/blocks/ai-chat/view.asset.php
+++ _inc/blocks/ai-chat/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-polyfill'), 'version' => 'eeebe7f9e8002bef46c3');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-polyfill'), 'version' => '30638f867628705154b3');
