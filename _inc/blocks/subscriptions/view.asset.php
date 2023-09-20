7c706a3e073841729e38b865cf2b70f16de3cf77
mashikag <mashikag@users.noreply.github.com>
2023-09-20 09:37:42 +0000

    Subscriptions Block: Do not append any excluded newsletter categories to the subscribe URL, when none categories are selected (#33181)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6246537955

--- _inc/blocks/subscriptions/view.asset.php
+++ _inc/blocks/subscriptions/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready', 'wp-polyfill'), 'version' => 'a561ceb835594acc0ede');
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-polyfill'), 'version' => '6ebc7b0ebcc88bc010aa');
