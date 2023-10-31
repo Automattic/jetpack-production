d0edf8e5c2b60039028166a8bad951b497d7e14b
haqadn <haqadn@users.noreply.github.com>
2023-10-31 11:14:34 +0000

    Components: Fix IconTooltip Popover (#33856)
    * Fix IconTooltip component
    
    * changelog
    
    * Use inline PopOver
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6705462039

--- jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
+++ jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '34f96f5a09dc03f00912');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '8ceb09074634c3386925');
