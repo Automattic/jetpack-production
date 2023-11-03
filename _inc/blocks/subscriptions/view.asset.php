dd01e21d3e573a965bac61a49d36310a2ebd8d85
lezama <lezama@users.noreply.github.com>
2023-11-03 12:26:50 +0000

    Subscribe Block: Introduce "Subscribed" state (#33897)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6745004211

--- _inc/blocks/subscriptions/view.asset.php
+++ _inc/blocks/subscriptions/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready', 'wp-polyfill'), 'version' => 'ca4446280cb53e27e816');
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-polyfill'), 'version' => '19ee265776f65f508e3b');
