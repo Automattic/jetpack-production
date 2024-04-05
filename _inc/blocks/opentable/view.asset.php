46154c0ebd6e6cebdad5de90d4074412159c4ad8
monsieur-z <monsieur-z@users.noreply.github.com>
2024-04-05 18:05:19 +0000

    Blocks: upgrade to API version 3 - Part 2 (#36742)
    * Bump blocks API version
    
    * Fix tests
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8573782120

--- _inc/blocks/opentable/view.asset.php
+++ _inc/blocks/opentable/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'd77e0e36af4de38f65b3');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'f23ee73c32d50167f7be');
