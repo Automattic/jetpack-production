9483e43d2054705985bcf1a9c89663111a33ce12
monsieur-z <monsieur-z@users.noreply.github.com>
2024-04-12 13:38:14 +0000

    Blocks: upgrade to API version 3 - Part 5 (#36854)
    * Bump Contact Info block API version
    
    * Bump Image Compare block API version
    
    * Bump Tiled Gallery block API version
    
    * Bump Related Posts block API version
    
    * Bump Pinterest block API version
    
    * Fix failing tests
    
    * Fix styling issue with Tiled Gallery
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/8662773804

--- _inc/blocks/tiled-gallery/view.asset.php
+++ _inc/blocks/tiled-gallery/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready', 'wp-polyfill'), 'version' => 'dc94b44629b817c982d5');
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-polyfill'), 'version' => '95c95e83df95e26774b2');
