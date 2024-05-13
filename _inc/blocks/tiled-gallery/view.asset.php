b4cf3d87ce760910a703df7320850b4626df9fef
ice9js <ice9js@users.noreply.github.com>
2024-05-13 14:05:15 +0000

    Update Jetpack version
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9064221524
    
    Upstream-Ref: Automattic/jetpack@d7e8a2d385fa69b2c4daf9c8703994776160a6ed

--- _inc/blocks/tiled-gallery/view.asset.php
+++ _inc/blocks/tiled-gallery/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready', 'wp-polyfill'), 'version' => 'c0a1be28ff72d2a27604');
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-polyfill'), 'version' => '9b24295592dfed97f278');
