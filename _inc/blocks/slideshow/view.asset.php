181fc87b2d4c5ff7bc44158273c79ffe32b320b3
anomiex <anomiex@users.noreply.github.com>
2024-05-15 21:23:38 +0000

    Lock file maintenance (#37348)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9102700990
    
    Upstream-Ref: Automattic/jetpack@c7f0e638af211d06047210be10c476eac53843c6

--- _inc/blocks/slideshow/view.asset.php
+++ _inc/blocks/slideshow/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready', 'wp-escape-html', 'wp-i18n', 'wp-polyfill'), 'version' => '167b332b7953575fbe1e');
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-escape-html', 'wp-i18n', 'wp-polyfill'), 'version' => 'f9e37fa65e3f40140615');
--- jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
+++ jetpack_vendor/automattic/jetpack-videopress/build/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '39ece253dada691f67cf');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '20760751384defb75cf8');
