87dd6280d124dd47fbb53a98ebf3b0126352853d
anomiex <anomiex@users.noreply.github.com>
2024-10-07 20:58:53 +0000

    Lock file maintenance (#39653)
    * Lock file maintenance
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11223651590
    
    Upstream-Ref: Automattic/jetpack@8443130ff4c164b2b87313f53d57aa5b8cbb2552

--- _inc/blocks/slideshow/view.asset.php
+++ _inc/blocks/slideshow/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready', 'wp-escape-html', 'wp-i18n', 'wp-polyfill'), 'version' => '697f325b61b33e1505cb');
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-escape-html', 'wp-i18n', 'wp-polyfill'), 'version' => '0cc92effc4d2340a355e');
