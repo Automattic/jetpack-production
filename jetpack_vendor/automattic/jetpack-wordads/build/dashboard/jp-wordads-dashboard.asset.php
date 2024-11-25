6b81ca999cd95ec02111a4901c81a3b55046a2ef
zinigor <zinigor@users.noreply.github.com>
2024-11-25 16:44:41 +0000

    Updated package versions for automattic/jetpack
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12014608453
    
    Upstream-Ref: Automattic/jetpack@61d8e5fb6c56546060ca36f5c072c5ca8ba1d297

--- jetpack_vendor/automattic/jetpack-wordads/build/dashboard/jp-wordads-dashboard.asset.php
+++ jetpack_vendor/automattic/jetpack-wordads/build/dashboard/jp-wordads-dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '25c9b920c5a9fa0f2d61');
+<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'e7bd20e513b43cfbc2d1');
