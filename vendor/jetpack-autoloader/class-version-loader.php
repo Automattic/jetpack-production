88a2c5876adf51a925d0586a0350e1a47855d4cc
zinigor <zinigor@users.noreply.github.com>
2024-11-26 12:59:13 +0000

    Updated package versions for automattic/jetpack
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12030998858
    
    Upstream-Ref: Automattic/jetpack@342e31e6ce9921127f59b70907f37a1467f6dd06

--- vendor/jetpack-autoloader/class-version-loader.php
+++ vendor/jetpack-autoloader/class-version-loader.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpf11009ded9fc4592b6a05b61ce272b3c_jetpackⓥ14_1_a_2\al3_1_3;
+namespace Automattic\Jetpack\Autoloader\jpf11009ded9fc4592b6a05b61ce272b3c_jetpackⓥ14_1_a_7\al5_0_0;
 
  // phpcs:ignore
 
