9d24e0cab4d7edc73732e76eb2556c9381ff5876
tbradsha <tbradsha@users.noreply.github.com>
2024-12-02 17:31:40 +0000

    Updated package versions for automattic/jetpack
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12124629172
    
    Upstream-Ref: Automattic/jetpack@72b41b4ccb85f17df6fed10d230cbefc0ed195c2

--- class.jetpack-gutenberg.php
+++ class.jetpack-gutenberg.php
@@ -1345,7 +1345,7 @@ class Jetpack_Gutenberg {
 	 * blocks register themselves on init.
 	 *
 	 * @static
-	 * @since 14.1-a.2
+	 * @since 14.1
 	 * @return void
 	 */
 	public static function register_block_metadata_collection() {
--- jetpack_vendor/automattic/jetpack-connection/dist/jetpack-connection.asset.php
+++ jetpack_vendor/automattic/jetpack-connection/dist/jetpack-connection.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '4e98b245f532e8762147');
+<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'e288951fbef3b68b97e5');
