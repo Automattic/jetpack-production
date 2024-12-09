e377705376b903842c579e6cc161fd607ec0b8a8
zinigor <zinigor@users.noreply.github.com>
2024-12-09 16:09:45 +0000

    Updated package versions for automattic/jetpack
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12239451681
    
    Upstream-Ref: Automattic/jetpack@7d2a8cc735171c2a03c38907c8af7623ba1d5f43

--- jetpack_vendor/automattic/jetpack-sync/src/class-defaults.php
+++ jetpack_vendor/automattic/jetpack-sync/src/class-defaults.php
@@ -79,6 +79,7 @@ class Defaults {
 		'jetpack_comment_form_color_scheme',
 		'jetpack_comment_likes_enabled',
 		'jetpack_excluded_extensions',
+		'jetpack_holiday_snow_enabled',
 		'jetpack_mailchimp',
 		'jetpack_options',
 		'jetpack_portfolio',
@@ -89,6 +90,7 @@ class Defaults {
 		'jetpack_relatedposts',
 		'jetpack_social_notes_config',
 		'jetpack_social_settings',
+		'jetpack_social_utm_settings',
 		'jetpack_sso_match_by_email',
 		'jetpack_sso_require_two_step',
 		'jetpack_sync_non_blocking', // is non-blocking Jetpack Sync flow enabled.
@@ -466,6 +468,7 @@ class Defaults {
 		'bigcommerce_task', // BigCommerce Plugin - Store import queue.
 		'secupress_log_err404', // SecuPress Plugin - Log 404 pages
 		'iw_omnibus_price_log', // Omnibus Plugin - Log price changes.
+		'od_url_metrics', // Optimization Detective - Log URL metrics.
 	);
 
 	/**
