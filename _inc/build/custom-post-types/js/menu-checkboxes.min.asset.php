56868aff50fdececc42cf05cada7199ffc9499a9
coder-karen <coder-karen@users.noreply.github.com>
2024-12-18 15:46:43 +0000

    Restaurant Menu CPT: Replace most jQuery with Javascript (#40645)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12396135207
    
    Upstream-Ref: Automattic/jetpack@829b8dc6b330ada0ceec66f9236d31721375542b

--- _inc/build/custom-post-types/js/menu-checkboxes.min.asset.php
+++ _inc/build/custom-post-types/js/menu-checkboxes.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'cb9c9ebc475573f08838');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '5fadb54ea9d5485c71c6');
