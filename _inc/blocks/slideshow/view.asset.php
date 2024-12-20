2a4842af0f9ed35d5c25d048d3d8d4f3c7f8fbcf
anomiex <anomiex@users.noreply.github.com>
2024-12-20 15:41:13 +0000

    Update pnpm to v9.15.0 [SECURITY] (#40560)
    * Update pnpm to v9.15.0 [SECURITY]
    * Bring back pnpmfile hack for pnpm/pnpm#3935, which regressed in 9.12.0
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brandon Kraft <public@brandonkraft.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12434014268
    
    Upstream-Ref: Automattic/jetpack@b414852ed3a3c265c199bbafc2666ce076c52c01

--- _inc/blocks/slideshow/view.asset.php
+++ _inc/blocks/slideshow/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready', 'wp-escape-html', 'wp-i18n', 'wp-polyfill'), 'version' => '16e27898c3cfa7efbd39');
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-escape-html', 'wp-i18n', 'wp-polyfill'), 'version' => '7b723ea2d17f45883a9d');
