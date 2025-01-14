52952df669a177ce2686850b24245898ba010304
simison <simison@users.noreply.github.com>
2025-01-14 12:14:13 +0000

    Revert "Forms: rename "URL" field to "Website" (#40994)" (#41026)
    This reverts commit d52d80b3bd46275f8fafc5594c900d77347b1e43.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12767136040
    
    Upstream-Ref: Automattic/jetpack@692d991780d6c5d29cd436715d0248cd015281f9

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-forms' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-forms',
-      'ver' => '0.35.0-alpha1736852339',
+      'ver' => '0.34.6',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
