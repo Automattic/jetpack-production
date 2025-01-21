56028126e58724efd61da5d5aaa89b4d2502b0c7
ntsekouras <ntsekouras@users.noreply.github.com>
2025-01-21 12:06:46 +0000

    Forms: Create new default block when pressing Enter on text inputs (#41177)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12886337068
    
    Upstream-Ref: Automattic/jetpack@a50939c87167a4428df461c0a24271b607fe825c

--- jetpack_vendor/automattic/jetpack-forms/CHANGELOG.md
+++ jetpack_vendor/automattic/jetpack-forms/CHANGELOG.md
@@ -9,6 +9,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 This is an alpha version! The changes listed here are not final.
 
+### Added
+- Forms: Create new default block when pressing Enter on text inputs
+
 ### Fixed
 - Forms: Add unique ids to each form
 
--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -50,7 +50,7 @@ return array(
     ),
     'jetpack-forms' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-forms',
-      'ver' => '0.35.1-alpha1737418199',
+      'ver' => '0.35.1-alpha1737460276',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
