51a56bbcd08fe609849f4ea7c7636376848fd418
sergeymitr <sergeymitr@users.noreply.github.com>
2025-02-24 18:04:57 +0000

    Connection: improve "connection owner unlink" modal responsiveness (#41998)
    Fix the bottom buttons panel height on tall screens.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13504424343
    
    Upstream-Ref: Automattic/jetpack@47a556aa2606045d14913fc0cc87678378d04182

--- jetpack_vendor/automattic/jetpack-connection/dist/jetpack-connection.asset.php
+++ jetpack_vendor/automattic/jetpack-connection/dist/jetpack-connection.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '51f69fbd939224c71267');
+<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'e7268943fcd0ec761ac6');
