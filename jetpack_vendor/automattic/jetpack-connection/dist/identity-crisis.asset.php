6b1d9205f1aa8192fd5e9ab521a1f2519b9b3c6a
htdat <htdat@users.noreply.github.com>
2025-03-07 07:30:41 +0000

    Add mainBodyTextDev for the body text of the Safe Mode notice in the dev mode (#42280)
    * Add mainBodyTextDev for the body text of the Safe Mode notice in the development mode
    
    * Slight update: 2 -> two
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13715676953
    
    Upstream-Ref: Automattic/jetpack@9ccfb90bf10301f60d9827344c72d0d770d6af8f

--- jetpack_vendor/automattic/jetpack-connection/dist/identity-crisis.asset.php
+++ jetpack_vendor/automattic/jetpack-connection/dist/identity-crisis.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'ebaea1c344c9057af976');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '8067c053c84ec34ca36a');
