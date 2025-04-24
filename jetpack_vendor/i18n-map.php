97821ad11ac75ada3019d401b0101255f6cf2c81
anomiex <anomiex@users.noreply.github.com>
2025-04-24 16:59:07 +0000

    phpunit: Update to yoast/phpunit-polyfills v4 (#43231)
    This brings in use of PHPUnit 12 for PHP 8.3+. Other than needing
    `phpunit.12.xml.dist` files (as symlinks to the PHPUnit 11 configs),
    this seems to work without issue after a few cleanups in #43215
    and #43217.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/14646946984
    
    Upstream-Ref: Automattic/jetpack@320d92e7a0e6be41533b5f88f6b2fd0cdc13f163

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -6,35 +6,35 @@ return array(
   'packages' => array(
     'jetpack-account-protection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-account-protection',
-      'ver' => '0.2.3-alpha1745501011',
+      'ver' => '0.2.3-alpha1745512883',
     ),
     'jetpack-admin-ui' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-admin-ui',
-      'ver' => '0.5.8-alpha1745352378',
+      'ver' => '0.5.8-alpha1745512883',
     ),
     'jetpack-assets' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-assets',
-      'ver' => '4.0.19-alpha1745352378',
+      'ver' => '4.0.19-alpha1745512883',
     ),
     'jetpack-backup-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-backup',
-      'ver' => '4.1.1-alpha1745506580',
+      'ver' => '4.1.1-alpha1745512883',
     ),
     'jetpack-blaze' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-blaze',
-      'ver' => '0.25.22-alpha1745436992',
+      'ver' => '0.25.22-alpha1745512883',
     ),
     'jetpack-boost-core' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-core',
-      'ver' => '0.3.9',
+      'ver' => '0.3.10-alpha1745512883',
     ),
     'jetpack-boost-speed-score' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-boost-speed-score',
-      'ver' => '0.4.8-alpha1745352378',
+      'ver' => '0.4.8-alpha1745512883',
     ),
     'jetpack-classic-theme-helper' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-classic-theme-helper',
-      'ver' => '0.12.0-alpha1745512000',
+      'ver' => '0.12.0-alpha1745512883',
     ),
     'jetpack-compat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-compat',
@@ -46,107 +46,107 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.11.1-alpha1745436992',
+      'ver' => '6.11.1-alpha1745512883',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
-      'ver' => '0.2.16',
+      'ver' => '0.2.17-alpha1745512883',
     ),
     'jetpack-external-media' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-external-media',
-      'ver' => '0.3.6-alpha1745436992',
+      'ver' => '0.3.6-alpha1745512883',
     ),
     'jetpack-forms' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-forms',
-      'ver' => '0.50.0-alpha1745506580',
+      'ver' => '0.50.0-alpha1745512883',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
-      'ver' => '0.7.12-alpha1745352378',
+      'ver' => '0.7.12-alpha1745512883',
     ),
     'jetpack-import' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-import',
-      'ver' => '0.9.7',
+      'ver' => '0.9.8-alpha1745512883',
     ),
     'jetpack-ip' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-ip',
-      'ver' => '0.4.9-alpha1745352378',
+      'ver' => '0.4.9-alpha1745512883',
     ),
     'jetpack-jitm' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-jitm',
-      'ver' => '4.2.14-alpha1745436992',
+      'ver' => '4.2.14-alpha1745512883',
     ),
     'jetpack-licensing' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-licensing',
-      'ver' => '3.0.9-alpha1745352378',
+      'ver' => '3.0.9-alpha1745512883',
     ),
     'jetpack-masterbar' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-masterbar',
-      'ver' => '0.17.0-alpha1745512000',
+      'ver' => '0.17.0-alpha1745512883',
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.13.0-alpha1745478261',
+      'ver' => '5.13.0-alpha1745512883',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
-      'ver' => '0.4.8-alpha1745352378',
+      'ver' => '0.4.8-alpha1745512883',
     ),
     'jetpack-plugins-installer' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-plugins-installer',
-      'ver' => '0.5.4',
+      'ver' => '0.5.5-alpha1745512883',
     ),
     'jetpack-post-list' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-post-list',
-      'ver' => '0.8.13-alpha1745352378',
+      'ver' => '0.8.13-alpha1745512883',
     ),
     'jetpack-protect-models' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-models',
-      'ver' => '0.5.5-alpha1745352378',
+      'ver' => '0.5.5-alpha1745512883',
     ),
     'jetpack-protect-status' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-protect-status',
-      'ver' => '0.5.10-alpha1745352378',
+      'ver' => '0.5.10-alpha1745512883',
     ),
     'jetpack-publicize-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-publicize',
-      'ver' => '0.64.2-alpha1745352378',
+      'ver' => '0.64.2-alpha1745512883',
     ),
     'jetpack-search-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-search',
-      'ver' => '0.47.23-alpha1745506580',
+      'ver' => '0.47.23-alpha1745512883',
     ),
     'jetpack-stats' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-stats',
-      'ver' => '0.16.2-alpha1745352378',
+      'ver' => '0.16.2-alpha1745512883',
     ),
     'jetpack-stats-admin' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-stats-admin',
-      'ver' => '0.25.3-alpha1745499513',
+      'ver' => '0.25.3-alpha1745512883',
     ),
     'jetpack-subscribers-dashboard' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-subscribers-dashboard',
-      'ver' => '0.1.8-alpha1745478261',
+      'ver' => '0.1.8-alpha1745512883',
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.11.1-alpha1745486636',
+      'ver' => '4.11.1-alpha1745512883',
     ),
     'jetpack-videopress-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-videopress',
-      'ver' => '0.28.3-alpha1745478261',
+      'ver' => '0.28.3-alpha1745512883',
     ),
     'jetpack-waf' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-waf',
-      'ver' => '0.24.3-alpha1745436992',
+      'ver' => '0.24.3-alpha1745512883',
     ),
     'jetpack-wordads' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-wordads',
-      'ver' => '0.4.21-alpha1745506580',
+      'ver' => '0.4.21-alpha1745512883',
     ),
     'woocommerce-analytics' => array(
       'path' => 'jetpack_vendor/automattic/woocommerce-analytics',
-      'ver' => '0.4.5',
+      'ver' => '0.4.6-alpha1745512883',
     ),
   ),
 );
