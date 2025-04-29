6ab8c107fff76c69ed3237ac4a601517b1fa1491
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2025-04-29 12:13:22 +0000

    Connection: Remove auth providers support - revert #42602 (#43241)
    * Connection: Remove auth providers support - revert #42602
    
    * Remove unused constant in my-jetpack
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/14730608317
    
    Upstream-Ref: Automattic/jetpack@8c8d8a62c6d02bef0ab6ec22cb17d602cd07651b

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -46,7 +46,7 @@ return array(
     ),
     'jetpack-connection' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-connection',
-      'ver' => '6.11.1',
+      'ver' => '6.11.2-alpha1745927742',
     ),
     'jetpack-explat' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-explat',
@@ -86,7 +86,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.13.1-alpha1745914717',
+      'ver' => '5.13.1-alpha1745927742',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
