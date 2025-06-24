a819894c209223842c8d8340d915bee1f58e3d2c
tbradsha <tbradsha@users.noreply.github.com>
2025-06-24 15:33:18 +0000

    Updated package versions for automattic/jetpack
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/15854687533
    
    Upstream-Ref: Automattic/jetpack@637260c70abed5190037db777cbfcc1283e4a7a0

--- jetpack_vendor/automattic/jetpack-forms/src/class-jetpack-forms.php
+++ jetpack_vendor/automattic/jetpack-forms/src/class-jetpack-forms.php
@@ -14,7 +14,7 @@ use Automattic\Jetpack\Forms\Dashboard\Dashboard;
  */
 class Jetpack_Forms {
 
-	const PACKAGE_VERSION = '2.0.1-alpha';
+	const PACKAGE_VERSION = '2.0.1';
 
 	/**
 	 * Load the contact form module.
