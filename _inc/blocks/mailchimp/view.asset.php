8c47855802ba98e7b17b45822d7b98e64565d2fb
anomiex <anomiex@users.noreply.github.com>
2025-07-17 18:32:43 +0000

    jetpack: Replace use of lodash in Mailchimp block view.js (#44354)
    This block, instead of using the full lodash, was importing
    `lodash/debounce` directly (and therefore embedding it). Since we're
    already bringing in the `debounce` package, use it here too.
    
    Also, remove a useless `removeEventListener()` call. It never worked
    because it's not passing the same function.
    
    The savings of 1841 bytes (uncompressed) isn't much, but it's something
    and it sets a better example.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/16352763453
    
    Upstream-Ref: Automattic/jetpack@2cff86c3dafa317bfcaf0076c6bbcde8e090310c

--- _inc/blocks/mailchimp/view.asset.php
+++ _inc/blocks/mailchimp/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready', 'wp-i18n', 'wp-polyfill'), 'version' => '763d373f89365b713975');
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-i18n', 'wp-polyfill'), 'version' => 'f2fcc0ee67961ab17f72');
