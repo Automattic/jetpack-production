c92556b1505d43d0ffa65ab8c3c75fba46b63be8
tbradsha <tbradsha@users.noreply.github.com>
2025-09-03 16:01:38 +0000

    Revert "Forms: move international phone input to production (#44922)" (#45060)
    This reverts commit f5ae5f9755d614598d780e7364eada36e47be4da.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/17438730638
    
    Upstream-Ref: Automattic/jetpack@5adef5e856f658b16d75517bd5df4f4f7b360c7e

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -66,7 +66,7 @@ return array(
     ),
     'jetpack-forms' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-forms',
-      'ver' => '6.3.0-alpha1756910499',
+      'ver' => '6.2.0',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
