84311476279d0e453de27199d6dd4141e3ad1d67
tbradsha <tbradsha@users.noreply.github.com>
2025-09-04 14:58:49 +0000

    Update readme.txt
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/17467818098
    
    Upstream-Ref: Automattic/jetpack@0862926f8a394650ccc684d6e18f7d110e713ea6

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -66,7 +66,7 @@ return array(
     ),
     'jetpack-forms' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-forms',
-      'ver' => '6.0.0',
+      'ver' => '6.2.0',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
@@ -98,7 +98,7 @@ return array(
     ),
     'jetpack-my-jetpack' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-my-jetpack',
-      'ver' => '5.25.0',
+      'ver' => '5.25.2',
     ),
     'jetpack-password-checker' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-password-checker',
@@ -146,7 +146,7 @@ return array(
     ),
     'jetpack-sync' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '4.21.0',
+      'ver' => '4.21.1',
     ),
     'jetpack-videopress-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-videopress',
