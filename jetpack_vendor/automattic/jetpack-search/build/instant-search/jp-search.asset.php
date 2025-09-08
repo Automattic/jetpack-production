6fa9f8f7601100c9d5c8e12aa7d2b2a7584a456f
tbradsha <tbradsha@users.noreply.github.com>
2025-09-08 16:19:39 +0000

    Lock file maintenance (#45096)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/17556851280
    
    Upstream-Ref: Automattic/jetpack@8e0c8ccf8ed1b0df17f6bd0d43ac10689f6e6c5b

--- jetpack_vendor/automattic/jetpack-search/build/instant-search/jp-search.asset.php
+++ jetpack_vendor/automattic/jetpack-search/build/instant-search/jp-search.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n', 'wp-jp-i18n-loader', 'wp-polyfill', 'wp-url'), 'version' => 'a70d04c07cbdb25d1ddd');
+<?php return array('dependencies' => array('wp-i18n', 'wp-jp-i18n-loader', 'wp-polyfill', 'wp-url'), 'version' => 'ce6dc50d165e5b6d02c8');
