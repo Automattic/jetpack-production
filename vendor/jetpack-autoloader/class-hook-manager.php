487163dca549f81590df354d9ce9b876204f80aa
tbradsha <tbradsha@users.noreply.github.com>
2025-09-30 17:43:49 +0000

    Updated package versions for automattic/jetpack
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/18138442722
    
    Upstream-Ref: Automattic/jetpack@1b5bff9b13f0c622c770acb97a996a62978c1b31

--- vendor/jetpack-autoloader/class-hook-manager.php
+++ vendor/jetpack-autoloader/class-hook-manager.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpf11009ded9fc4592b6a05b61ce272b3c_jetpackⓥ15_1_a_10\al5_0_10;
+namespace Automattic\Jetpack\Autoloader\jpf11009ded9fc4592b6a05b61ce272b3c_jetpackⓥ15_1_a_11\al5_0_10;
 
  // phpcs:ignore
 
