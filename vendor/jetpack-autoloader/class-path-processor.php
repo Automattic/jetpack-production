c4565130a75c4181fe40f0aa63be09aba59c5a97
gmjuhasz <gmjuhasz@users.noreply.github.com>
2025-12-04 12:48:34 +0000

    Update readme.txt
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/19929275403
    
    Upstream-Ref: Automattic/jetpack@4194f28385ce8754c8d60fa340a0e6ba0bea5031

--- vendor/jetpack-autoloader/class-path-processor.php
+++ vendor/jetpack-autoloader/class-path-processor.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpf11009ded9fc4592b6a05b61ce272b3c_jetpackⓥ15_3\al5_0_13;
+namespace Automattic\Jetpack\Autoloader\jpf11009ded9fc4592b6a05b61ce272b3c_jetpackⓥ15_3_1\al5_0_13;
 
  // phpcs:ignore
 
