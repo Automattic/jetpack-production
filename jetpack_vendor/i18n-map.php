9bcab4f026a94c763c540ae21ff56fd390911f41
edanzer <edanzer@users.noreply.github.com>
2026-01-21 21:46:54 +0000

    Forms: add permanent delete for form posts (#46711)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/21226405530
    
    Upstream-Ref: Automattic/jetpack@7b37896207678b206df041e7767fbb68e6b0956d

--- jetpack_vendor/i18n-map.php
+++ jetpack_vendor/i18n-map.php
@@ -66,7 +66,7 @@ return array(
     ),
     'jetpack-forms' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-forms',
-      'ver' => '7.5.0-alpha1769025747',
+      'ver' => '7.5.0-alpha1769031001',
     ),
     'jetpack-image-cdn' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-image-cdn',
