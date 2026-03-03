bd38f7195fc6243498aa3d55b7b0ee24ae84b4a0
gmjuhasz <gmjuhasz@users.noreply.github.com>
2026-03-03 09:29:41 +0000

    Update stable tag
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/22616330990
    
    Upstream-Ref: Automattic/jetpack@8d5e9682ce3d24e5e8cbf4b096c590889b9afbde

--- vendor/jetpack-autoloader/class-shutdown-handler.php
+++ vendor/jetpack-autoloader/class-shutdown-handler.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpf11009ded9fc4592b6a05b61ce272b3c_jetpackⓥ15_6_beta\al5_0_16;
+namespace Automattic\Jetpack\Autoloader\jpf11009ded9fc4592b6a05b61ce272b3c_jetpackⓥ15_6\al5_0_16;
 
  // phpcs:ignore
 
