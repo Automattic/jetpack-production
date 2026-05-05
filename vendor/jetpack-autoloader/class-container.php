44889caa37b2cd379a42679109cb1b28a795563a
tbradsha <tbradsha@users.noreply.github.com>
2026-05-05 17:24:52 +0000

    Bump versions, squash changelog, and copy to readme
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/25391066212
    
    Upstream-Ref: Automattic/jetpack@6ab8e3ca5fb53008476152b7b37fa5cd38fe16f1

--- vendor/jetpack-autoloader/class-container.php
+++ vendor/jetpack-autoloader/class-container.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpf11009ded9fc4592b6a05b61ce272b3c_jetpackⓥ15_8_beta\al5_0_17;
+namespace Automattic\Jetpack\Autoloader\jpf11009ded9fc4592b6a05b61ce272b3c_jetpackⓥ15_8\al5_0_17;
 
  // phpcs:ignore
 
