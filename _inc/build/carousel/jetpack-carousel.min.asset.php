bffedc6756e2eea77a78bf70d2d137e68ead89b4
coder-karen <coder-karen@users.noreply.github.com>
2026-07-08 12:08:22 +0000

    Fix: Normalize event target for Carousel on attachment links (#39926) (#50220)
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/28940672102
    
    Upstream-Ref: Automattic/jetpack@fb60308663de2bb65b53c55f8541382f2ee08c04

--- _inc/build/carousel/jetpack-carousel.min.asset.php
+++ _inc/build/carousel/jetpack-carousel.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '2b8403c0e86af025a24f');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'befa4ff9d03fb7976a59');
