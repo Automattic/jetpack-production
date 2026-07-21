ccc2531fd18f1f1a0ad8bc3c320e9e5914b31466
coder-karen <coder-karen@users.noreply.github.com>
2026-07-21 13:22:42 +0000

    Connection: Component updates pt 4 - Basic and Required Plan screens (#50663)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/29833365660
    
    Upstream-Ref: Automattic/jetpack@126452bacbeb5845f5fdddf98975bdeb0b6436da

--- jetpack_vendor/automattic/jetpack-connection/dist/jetpack-connection.asset.php
+++ jetpack_vendor/automattic/jetpack-connection/dist/jetpack-connection.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-a11y', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '5b868b8020c9be18484b');
+<?php return array('dependencies' => array('jetpack-script-data', 'react', 'react-jsx-runtime', 'wp-a11y', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '2c9ad1284b54ddf19bae');
