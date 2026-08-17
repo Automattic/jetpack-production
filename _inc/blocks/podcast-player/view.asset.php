d103f0a7c69b9004fbb0a54eea9e2453a6469d8c
Nikschavan <Nikschavan@users.noreply.github.com>
2026-08-17 10:13:32 +0000

    Stats: point the readme at the Stats packages and the Calypso dashboard source (#51317)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/32018137080
    
    Upstream-Ref: Automattic/jetpack@e53c7ba912bfbc37dece702313c6be9dab5241eb

--- _inc/blocks/podcast-player/view.asset.php
+++ _inc/blocks/podcast-player/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-a11y', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '9c5402a2dbccd19a9692');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-a11y', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'ff668ea0895a3b889e63');
