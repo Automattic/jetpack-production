722c4d5a784ba69e288c2b54529be8943d2272b3
tbradsha <tbradsha@users.noreply.github.com>
2026-09-21 21:17:43 +0000

    Map block: Clean up marker SVG attributes (#52577)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35654695036
    
    Upstream-Ref: Automattic/jetpack@6e81cb3715e1fa4d4c96363c223ab5faeb9f65be

--- _inc/blocks/map/view.asset.php
+++ _inc/blocks/map/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-dom-ready', 'wp-polyfill'), 'version' => 'e6c6ed22d3b10da7a9e7');
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-polyfill'), 'version' => 'c2c2771cceb72674ff93');
